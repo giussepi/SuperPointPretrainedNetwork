535dd09e4c6d8edf6b56807b47cc716cbaca06b6
Giussepi <giussepexy@gmail.com>
2025-07-18 14:52:21 +0100

    tweak to use gauss2 model

---
+++ models/gauss2/model.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+""" models/gauss2/model.py """
+
+import torch
+from torch import nn
+
+from models.gauss2.unet_parts import inconv, down
+
+
+__all__ = [
+    'SuperPointNet_gauss2',
+]
+
+
+class SuperPointNet_gauss2(torch.nn.Module):
+    """ Pytorch definition of SuperPoint Network. """
+
+    def __init__(self, subpixel_channel=1):
+        super().__init__()
+        c1, c2, c3, c4, c5, d1 = 64, 64, 128, 128, 256, 256
+        det_h = 65
+        self.inc = inconv(1, c1)
+        self.down1 = down(c1, c2)
+        self.down2 = down(c2, c3)
+        self.down3 = down(c3, c4)
+        # self.down4 = down(c4, 512)
+        # self.up1 = up(c4+c3, c2)
+        # self.up2 = up(c2+c2, c1)
+        # self.up3 = up(c1+c1, c1)
+        # self.outc = outconv(c1, subpixel_channel)
+        self.relu = torch.nn.ReLU(inplace=True)
+        # self.outc = outconv(64, n_classes)
+        # Detector Head.
+        self.convPa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+        self.bnPa = nn.BatchNorm2d(c5)
+        self.convPb = torch.nn.Conv2d(c5, det_h, kernel_size=1, stride=1, padding=0)
+        self.bnPb = nn.BatchNorm2d(det_h)
+        # Descriptor Head.
+        self.convDa = torch.nn.Conv2d(c4, c5, kernel_size=3, stride=1, padding=1)
+        self.bnDa = nn.BatchNorm2d(c5)
+        self.convDb = torch.nn.Conv2d(c5, d1, kernel_size=1, stride=1, padding=0)
+        self.bnDb = nn.BatchNorm2d(d1)
+        self.output = None
+
+    def forward(self, x):
+        """ Forward pass that jointly computes unprocessed point and descriptor
+        tensors.
+        Input
+          x: Image pytorch tensor shaped N x 1 x patch_size x patch_size.
+        Output
+          semi: Output point pytorch tensor shaped N x 65 x H/8 x W/8.
+          desc: Output descriptor pytorch tensor shaped N x 256 x H/8 x W/8.
+        """
+        # Let's stick to this version: first BN, then relu
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+
+        # Detector Head.
+        cPa = self.relu(self.bnPa(self.convPa(x4)))
+        semi = self.bnPb(self.convPb(cPa))
+        # Descriptor Head.
+        cDa = self.relu(self.bnDa(self.convDa(x4)))
+        desc = self.bnDb(self.convDb(cDa))
+
+        dn = torch.norm(desc, p=2, dim=1)  # Compute the norm.
+        desc = desc.div(torch.unsqueeze(dn, 1))  # Divide by norm to normalize.
+        # output = {'semi': semi, 'desc': desc}
+        # self.output = output
+
+        # return output
+        return semi, desc
